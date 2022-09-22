d3a1183acae43c901c05f9f24c529f1285932611
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-22 21:12:50 +0300

    Complete add to cart deliverable

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+  const buttonValue = inCart ? "Remove From Cat" : "Add to Cart"
+  const buttonClass = inCart ? "" : "add"
+
+  function toggleInCart(){
+    setInCart(!inCart)
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={toggleInCart}>{buttonValue}</button>
     </li>
   );
 }
